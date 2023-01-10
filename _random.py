5cef065e38bc0990387eb0c454a2dfdaaec3e4d7
Marina Iglesia Pérez <iglesiaperezmarina@gmail.com>
2023-01-10 13:00:35 +0100

    Actualización

---
+++ _random.py
@@ -0,0 +1,8 @@
+import random
+
+
+for i in range (0, 10):
+    numero=random.random()
+    print(numero)
+    if numero>0.6 and numero<0.8:
+        print("hola")
--- dragon.py
+++ dragon.py
@@ -5,7 +5,7 @@
 
 # Mejoras:
 # - Mientras el usuario no seleccione una opcion valida no debe dejarle continuar
-# - Los ataques fallaran un 30% de las veces
+# - Los ataques del usuario fallaran un 30% de las veces
 # - La vida del usuario y del dragon nunca puede superar 100 ni ser inferior a 0
 # - 4 opcion: le cura la vida entera pero solo lo puede usar una vez (para el usuario). 
 # - El ataque del dragon va a valer 30, pero falla un 40% (siempre)
@@ -16,86 +16,123 @@
 
 import random
 
-def turno_usuario(vida): # Se puede llamar como quiera ()
+def turno_usuario(vida, veces_4): # Cuando llame a la funcion el parametro se puede llamar como quiera (vida)
     print("\nTu turno")
     print("1. Atacar")
     print("2. Curar")
     print("3. Defender")
+    print("4. Cura vida entera")
     usuario_opcion=int(input("Selecciona una opción: ") )   
-    no_es_opcion_1=usuario_opcion!=1
-    no_es_opcion_2=usuario_opcion!=2
-    no_es_opcion_3=usuario_opcion!=3
-    es_opcion_2_y_vida_100=usuario_opcion==2 and vida>95
+    es_la_opcion_1 = usuario_opcion==1
+    es_la_opcion_2 = usuario_opcion==2
+    es_la_opcion_3 = usuario_opcion==3
+    es_la_opcion_4 = usuario_opcion==4
+    es_opcion_2_y_vida_100 = usuario_opcion==2 and vida>95
+    es_opcion_4_y_ya_la_ha_usado = usuario_opcion == 4 and veces_4 >= 1
+    es_opcion_4_y_vida_100 = usuario_opcion ==4 and vida==100
 
 
-    while no_es_opcion_1 and no_es_opcion_2 and no_es_opcion_3 or es_opcion_2_y_vida_100:
+    while not (es_la_opcion_1 or es_la_opcion_2 or es_la_opcion_3 or es_la_opcion_4) or es_opcion_2_y_vida_100 or es_opcion_4_y_ya_la_ha_usado or es_opcion_4_y_vida_100:
         if es_opcion_2_y_vida_100:
             print("No puedes elegir la opción 'Curar' porque superarías los 100 puntos")
-        else:    
-            print("La opción introducida no es correcta")
+        elif es_opcion_4_y_ya_la_ha_usado:
+            print("No puedes usar esta opción porque ya la has usado")
+        elif es_opcion_4_y_vida_100:
+            print("No puedes usar esta opción porque ya tienes los 100 puntos")     
+        else:
+            print("La opción introducida no es correcta")  
+
         usuario_opcion=int(input("Selecciona una opción: ") )  
-        no_es_opcion_1=usuario_opcion!=1
-        no_es_opcion_2=usuario_opcion!=2
-        no_es_opcion_3=usuario_opcion!=3
-        es_opcion_2_y_vida_100=usuario_opcion==2 and vida>95 
+        es_la_opcion_1 = usuario_opcion==1
+        es_la_opcion_2 = usuario_opcion==2
+        es_la_opcion_3 = usuario_opcion==3 
+        es_la_opcion_4 = usuario_opcion==4
+        es_opcion_2_y_vida_100 = usuario_opcion==2 and vida>95 
+        es_opcion_4_y_ya_la_ha_usado = usuario_opcion == 4 and veces_4 >= 1
+        es_opcion_4_y_vida_100 = usuario_opcion ==4 and vida==100
+
     return usuario_opcion
 
 def turno_dragon(vida):
-    numero_aleatorio = random.randint(1, 3)   
-    es_opcion_2_y_vida_100=numero_aleatorio==2 and vida>95
-
-    while es_opcion_2_y_vida_100:
-        numero_aleatorio = random.randint(1, 2)
-        numero_aleatorio+=1
+    if vida>95:
+        dragon_opcion = random.choice([1,3])
     else:
-         numero_aleatorio = random.randint(1, 3)   
+        dragon_opcion = random.choice([1, 2, 3])    
+      
+        
+    print(f"Elección del dragón: {dragon_opcion}")       
+
+    return dragon_opcion
+
 
         
-        
 
-    print(f"Elección del dragón: {numero_aleatorio}")       
-
-    return numero_aleatorio
+  
 
 def play ():
+    veces_opcion_4=0
     vida_usuario=100
     vida_dragon=100 
     dragon_opcion=None
+    ataque_dragon=30
+    ataque_usuario=10
+    probabilidad_fallo_dragon=0.4
+    probabilidad_fallo_usuario=0.3
+    cura_dragon=5
+    cura_usuario=5
+
 
 
 
     while vida_usuario>0 and vida_dragon>0:
 
-        usuario_opcion=turno_usuario(vida_usuario) #TURNO USUARIO
+        usuario_opcion=turno_usuario(vida_usuario, veces_opcion_4) #TURNO USUARIO
         if usuario_opcion==1:
-            if dragon_opcion==3:  # Usuario ataca (1), dragón se defiende(3)
-                print("Has atacado (1) al dragón pero se ha defendido (3) y no le has hecho daño")
-            else:    
-                vida_dragon-=10
-                print("Has atacado (1) al dragón y le has hecho daño")
+            if random.random() > probabilidad_fallo_usuario: #Si no falla
+                if dragon_opcion==3:  # Usuario ataca (1), dragón se defiende(3)
+                    print("Has atacado (1) al dragón pero se ha defendido (3) y no le has hecho daño")
+                else:    
+                    vida_dragon-=ataque_usuario
+                    print("Has atacado (1) al dragón y le has hecho daño")
+            else: #Si falla
+                print("Tu ataque ha fallado, no has hecho daño al dragón")        
+
         elif usuario_opcion==2:
-            if vida_usuario<100:
-                vida_usuario+=5
-                print("Te has curado (2) y has ganado 5 puntos")
+            
+            vida_usuario+=cura_usuario
+            print("Te has curado (2) y has ganado 5 puntos")
             
         elif usuario_opcion==3:
-            print("Te has defendido (3)")    
+            print("Te has defendido (3)")   
+
+        # TODO: Hacer que el usuario no pierda el turno cuando seleccione opcion 4
+        elif usuario_opcion==4:     
+            print("Te has curado y tienes el 100% de vida")
+            vida_usuario=100
+            veces_opcion_4+=1
+
+            
+
 
         print(f"El dragón tiene {vida_dragon} puntos de vida")
 
         if vida_dragon<=0:
+            print("Has ganado")    
             break
 
-        dragon_opcion=turno_dragon()  #TURNO DRAGON
+        dragon_opcion=turno_dragon(vida_dragon)  #TURNO DRAGON
         if dragon_opcion==1: 
-            print("El dragón te ataca (1)")
-            if usuario_opcion==3:
-                print("...pero no te ha hecho daño porque te defendiste (3)")  
-            else:      
-                vida_usuario-=10
-        
+            if random.random() > probabilidad_fallo_dragon:  #Si no falla
+                print("El dragón te ataca (1)")
+                if usuario_opcion==3:
+                    print("...pero no te ha hecho daño porque te defendiste (3)")  
+                else:      
+                    vida_usuario-=ataque_dragon
+            else:  #Si falla
+                print("El dragón ha fallado y no te ha hecho daño")        
+            
         elif dragon_opcion==2:
-            vida_dragon+=5   
+            vida_dragon+=cura_dragon 
             print("El dragón se ha curado (2) y ha ganado 5 puntos") 
         elif dragon_opcion==3:
             print("El dragón se ha defendido (3)")
@@ -109,7 +146,9 @@ def play ():
     if vida_usuario<=0:
         print("Has perdido")
 
-    if vida_dragon<=0:
-        print("Has ganado")    
+  
 
 play()
+
+
+
